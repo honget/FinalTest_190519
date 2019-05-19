4d5a26d1a9b7174f5caa0a36fce14311822ea97f
Hong <bilre84@naver.com>
2019-05-19 12:51:46 +0900

    Initial commit

---
+++ app/src/main/java/com/example/finaltest_190519/LoginActivity.java
@@ -0,0 +1,113 @@
+package com.example.finaltest_190519;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.example.finaltest_190519.databinding.ActivityLoginBinding;
+import com.example.finaltest_190519.utills.CommectSever;
+import com.example.finaltest_190519.utills.ContextUtill;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class LoginActivity extends BaseActivity {
+
+    ActivityLoginBinding loginAct;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        bindViews();
+        setValues();
+        setupEvents();
+    }
+
+    @Override
+    public void setupEvents() {
+
+        loginAct.submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String id = loginAct.usrIdEdt.getText().toString();
+                String pw = loginAct.usrPwEdt.getText().toString();
+                CommectSever.postRequestSingIn(mContext, id, pw, new CommectSever.JsonResponsHandler() {
+                    @Override
+                    public void onResponse(JSONObject json) {
+
+                        try {
+                            if(json.getInt("code") == 200){
+
+                                JSONObject data = json.getJSONObject("data");
+                                String token = data.getString("token");
+
+                                ContextUtill.setUserToken(mContext, token);
+
+                                moveMainPage(token);
+
+                            }else{
+                                AlertDialog.Builder alert = new AlertDialog.Builder(mContext);
+
+                                alert.setTitle("로그인 실패");
+                                alert.setMessage(json.getString("message"));
+                                alert.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        loginAct.usrPwEdt.setText("");
+                                    }
+                                });
+
+                                final AlertDialog.Builder finalAlert = alert;
+
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        finalAlert.show();
+                                    }
+                                });
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                });
+
+            }
+        });
+    }
+
+    @Override
+    public void setValues() {
+
+        String userToken =ContextUtill.getUserToken(mContext);
+
+        Log.d("로그인 처리 : ", userToken);
+//
+//        if(userToken != null){
+//
+//            moveMainPage(userToken);
+//        }
+    }
+
+    public void moveMainPage(String userToken){
+
+        Log.d("로그인 처리 : ", userToken);
+        Intent intent = new Intent(mContext, MainActivity.class);
+        intent.putExtra("USER_TOKEN", userToken);
+
+        startActivity(intent);
+
+        finish();
+    }
+
+    @Override
+    public void bindViews() {
+        loginAct = DataBindingUtil.setContentView(this, R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/finaltest_190519/utills/ContextUtill.java
@@ -0,0 +1,69 @@
+package com.example.finaltest_190519.utills;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class ContextUtill {
+
+    /**
+     * 메모 파일정보 변수
+     */
+    private  static final String PREF_NM = "LoginServerExamplePref";
+
+    /**
+     * 이 메모에서 다루는 항목들의 리스트를 변수로 생성.
+     */
+    private  static final String USER_INPUT_ID = "USER_INPUT_ID";
+    private  static final String USER_TOKEN = "USER_TOKEN";
+
+    /**
+     * 실제로 각각의 항목을 저장/불러오는 기능
+     */
+
+     // setter : ID를 저장하는 id_setter
+    public static void setUserInputId(Context context, String inputId){
+
+        // 메모장 파일을 여는 작업
+        SharedPreferences pref = context.getSharedPreferences(PREF_NM, Context.MODE_PRIVATE);
+
+        // 실제 데이터 저장
+
+        //수정 시작
+        pref.edit().putString(USER_INPUT_ID, inputId).apply();
+
+    }
+
+    // getter : 저장된 id 있으면 불러오는 기능
+    public static String getUserInputId(Context context){
+
+        // 메모장 파일을 여는 작업
+        SharedPreferences pref = context.getSharedPreferences(PREF_NM, Context.MODE_PRIVATE);
+
+        // 키, null 일경우 값
+        return pref.getString(USER_INPUT_ID, "");
+    }
+
+    // setter : TOKEN를 저장하는 token_setter
+    public static void setUserToken(Context context, String inputToken){
+
+        // 메모장 파일을 여는 작업
+        SharedPreferences pref = context.getSharedPreferences(PREF_NM, Context.MODE_PRIVATE);
+
+        // 실제 데이터 저장
+
+        //수정 시작
+        pref.edit().putString(USER_TOKEN, inputToken).apply();
+
+    }
+
+    // getter : 저장된 Token 있으면 불러오는 기능
+    public static String getUserToken(Context context){
+
+        // 메모장 파일을 여는 작업
+        SharedPreferences pref = context.getSharedPreferences(PREF_NM, Context.MODE_PRIVATE);
+
+        // 키, null 일경우 값
+        return pref.getString(USER_TOKEN, "");
+    }
+
+}
